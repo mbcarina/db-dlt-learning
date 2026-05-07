1ed590430dd60e14cdb6d83f68773e370e959664
Mohamed Bassirou CISSE <mohamed.cisse@carina.consulting>
2026-05-07 13:26:32 +0200

    Exclude integration tests from PR CI

--- tests/sample_taxis_test.py
+++ tests/sample_taxis_test.py
@@ -1,8 +1,7 @@
-from databricks.sdk.runtime import spark
-from pyspark.sql import DataFrame
+import pytest
 from dab_dlt_learning import taxis
 
-
+@pytest.mark.integration
 def test_find_all_taxis():
     results = taxis.find_all_taxis()
     assert results.count() > 5
